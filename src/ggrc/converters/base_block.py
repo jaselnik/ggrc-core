b3e2a9a86a15120ceab01b6af643b091b4bf8f62
Denis Tamkovich <tamkovichdenis@gmail.com>
2019-11-13 16:13:21 +0300

    Remove reloading cached cads

--- src/ggrc/converters/base_block.py
+++ src/ggrc/converters/base_block.py
@@ -120,11 +120,7 @@ class BlockConverter(object):
         self._ca_definitions_cache = self._create_ca_definitions_cache()
     return self._ca_definitions_cache
 
-  def row_ca_definition_data(self, *args, **kwargs):
-    """Get custom attribute definitions data used in specified row or block"""
-    return self._create_ca_definitions_cache(*args, **kwargs)
-
-  def _create_ca_definitions_cache(self, field_names=None, object_ids=None):
+  def _create_ca_definitions_cache(self, field_names=None):
     """Create cache for custom attribute definitions used in this block."""
     if not issubclass(self.object_class, (mixins.CustomAttributable,
                                           mixins.ExternalCustomAttributable)):
@@ -153,11 +149,10 @@ class BlockConverter(object):
     elif field_names:
       field_names = map(_strip_ca_prefix, filter(_is_ca, field_names))
 
-    object_ids = object_ids or self.object_ids
     if hasattr(self.object_class, 'clean_cad_fields'):
       field_names = self.object_class.clean_cad_fields(field_names)
     ca_definitions = self.object_class.get_custom_attribute_definitions(
-        attributable_ids=object_ids or None,
+        attributable_ids=self.object_ids or None,
         field_names=field_names or None,
     )
 
