ecab01d0f453b3d76494eefdef82f51d4c88382f
Denis Tamkovich <tamkovichdenis@gmail.com>
2020-02-13 13:37:23 +0300

    Remove ability to create/update/delete Roles S&R objects via GGRC API

--- test/integration/ggrc/access_control/test_access_control_role.py
+++ test/integration/ggrc/access_control/test_access_control_role.py
@@ -20,6 +20,9 @@ MANDATORY_ROLE_RESPONSE = {
     "Program": {"row_warnings": {errors.OWNER_MISSING.format(
         line=3, column_name=ROLE_NAME)}}}
 NON_MANDATORY_ROLE_RESPONSE = {}
+SYNCED_DEFINITIONS = list(
+    model.__name__ for model in all_models.get_external_models()
+)
 
 
 @ddt.ddt
@@ -155,22 +158,22 @@ class TestAccessControlRole(TestCase):
         "Forbidden error should be thrown when non-editable " \
         "role {} deleted.".format(ac_role.name)
 
-  @ddt.data("Control")
+  @ddt.data(*SYNCED_DEFINITIONS)
   def test_create_from_ggrcq(self, object_type):
-    """Test that create action only for GGRCQ."""
+    """Test that create {0.__name__} role action only for GGRCQ."""
     with self.api.as_external():
       response = self._post_role(object_type=object_type)
       self.assertEqual(response.status_code, 201)
 
-  @ddt.data("Control")
+  @ddt.data(*SYNCED_DEFINITIONS)
   def test_create_from_ggrc(self, object_type):
-    """Test create action not allowed for GGRC."""
+    """Test create {0.__name__} role action not allowed for GGRC."""
     response = self._post_role(object_type=object_type)
     self.assertEqual(response.status_code, 405)
 
-  @ddt.data("Control")
+  @ddt.data(*SYNCED_DEFINITIONS)
   def test_modify_from_ggrcq(self, object_type):
-    """Test that modify action only for GGRCQ."""
+    """Test that modify {0.__name__} role action only for GGRCQ."""
     with factories.single_commit():
       acr_id = factories.AccessControlRoleFactory(object_type=object_type).id
 
@@ -179,18 +182,18 @@ class TestAccessControlRole(TestCase):
       response = self.api.put(acr, {"name": "new acr"})
       self.assertEqual(response.status_code, 200)
 
-  @ddt.data("Control")
+  @ddt.data(*SYNCED_DEFINITIONS)
   def test_modify_from_ggrc(self, object_type):
-    """Test modify action not allowed for GGRC."""
+    """Test modify {0.__name__} role action not allowed for GGRC."""
     with factories.single_commit():
       acr = factories.AccessControlRoleFactory(object_type=object_type)
 
     response = self.api.put(acr, {"name": "new acr"})
     self.assertEqual(response.status_code, 405)
 
-  @ddt.data("Control")
+  @ddt.data(*SYNCED_DEFINITIONS)
   def test_delete_from_ggrcq(self, object_type):
-    """Test that modify action only for GGRCQ."""
+    """Test that modify {0.__name__} role action only for GGRCQ."""
     with factories.single_commit():
       acr_id = factories.AccessControlRoleFactory(object_type=object_type).id
 
@@ -199,9 +202,9 @@ class TestAccessControlRole(TestCase):
       response = self.api.delete(acr)
       self.assertEqual(response.status_code, 200)
 
-  @ddt.data("Control")
+  @ddt.data(*SYNCED_DEFINITIONS)
   def test_delete_from_ggrc(self, object_type):
-    """Test modify action not allowed for GGRC."""
+    """Test modify {0.__name__} role action not allowed for GGRC."""
     with factories.single_commit():
       acr = factories.AccessControlRoleFactory(object_type=object_type)
 
