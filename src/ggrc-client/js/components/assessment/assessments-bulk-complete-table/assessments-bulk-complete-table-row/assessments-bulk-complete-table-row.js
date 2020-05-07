437c372f8a69d6255f02e0e55435a53197535fec
Iryna Malash <mal7rish@gmail.com>
2020-05-07 19:25:01 +0300

    Create <assessments-bulk-complete-table-row> component

---
+++ src/ggrc-client/js/components/assessment/assessments-bulk-complete-table/assessments-bulk-complete-table-row/assessments-bulk-complete-table-row.js
@@ -0,0 +1,21 @@
+/*
+  Copyright (C) 2020 Google Inc.
+  Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+*/
+
+import canComponent from 'can-component';
+import canDefineMap from 'can-define/map/map';
+import canStache from 'can-stache';
+import template from './assessments-bulk-complete-table-row.stache';
+
+const ViewModel = canDefineMap.extend({seal: false}, {
+  rowData: {
+    value: () => [],
+  },
+});
+
+export default canComponent.extend({
+  tag: 'assessments-bulk-complete-table-row',
+  view: canStache(template),
+  ViewModel,
+});
--- src/ggrc-client/js/components/assessment/assessments-bulk-complete-table/assessments-bulk-complete-table.js
+++ src/ggrc-client/js/components/assessment/assessments-bulk-complete-table/assessments-bulk-complete-table.js
@@ -8,6 +8,7 @@ import canDefineMap from 'can-define/map/map';
 import canStache from 'can-stache';
 import template from './assessments-bulk-complete-table.stache';
 import './assessments-bulk-complete-table-header/assessments-bulk-complete-table-header';
+import './assessments-bulk-complete-table-row/assessments-bulk-complete-table-row';
 
 const ViewModel = canDefineMap.extend({seal: false}, {
   assessmentsList: {
@@ -25,6 +26,36 @@ const ViewModel = canDefineMap.extend({seal: false}, {
         };
       });
     },
+  },
+  rowsData: {
+    get() {
+      return this.buildRowsData();
+    },
+  },
+  buildRowsData() {
+    let data = [];
+
+    this.assessmentsList.forEach((assessment) => {
+      const asmtData = {
+        asmtTitle: assessment.title,
+        asmtId: assessment.id,
+        asmtStatus: assessment.status,
+        asmtType: assessment.assessment_type,
+      };
+      let attrData = [];
+      this.attributesList.forEach((attribute) => {
+        attrData.push({
+          attrTitle: attribute.title,
+          value: attribute.values[assessment.id]
+            ? attribute.values[assessment.id].value
+            : 'Not applicable',
+        });
+      });
+
+      data.push({attributes: attrData, ...asmtData});
+    });
+
+    return data;
   },
 });
 
