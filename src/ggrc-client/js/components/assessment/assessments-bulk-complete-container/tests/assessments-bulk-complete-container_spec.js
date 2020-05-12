baf2f18f5a1537e64459e587c7643f209a8387da
natalya-mihalyonok <natallia_mikhalenak@epam.com>
2020-05-12 12:37:59 +0300

    Add unit tests for 'Save answers' button

--- src/ggrc-client/js/components/assessment/assessments-bulk-complete-container/tests/assessments-bulk-complete-container_spec.js
+++ src/ggrc-client/js/components/assessment/assessments-bulk-complete-container/tests/assessments-bulk-complete-container_spec.js
@@ -115,6 +115,19 @@ describe('assessments-bulk-complete-container component', () => {
       expect(viewModel.isLoading).toBe(false);
     });
   });
+
+  describe('onSaveAnswersClick() method', () => {
+    it('sets "isAttributeModified" to false', async () => {
+      viewModel.isAttributeModified = true;
+      spyOn(backendGdriveClient, 'withAuth')
+        .and.returnValue(Promise.resolve({id: 1}));
+      spyOn(viewModel, 'trackBackgroundTask');
+      await viewModel.onSaveAnswersClick();
+
+      expect(viewModel.isAttributeModified).toBeFalsy();
+    });
+  });
+
   describe('onCompleteClick() method', () => {
     beforeEach(() => {
       spyOn(ModalsUtils, 'confirm');
